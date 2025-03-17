8706c9b46be0ccf0e27b744a98390603c520a4a7
João Andreassa <joaovra@gmail.com>
2025-03-17 18:49:25 -0300

    commit

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,22 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let currentState = { ...state }; // Copia inicial do estado
+  const stateHistory = [];
+
+  for (const action of actions) {
+      if (action.type === 'clear') {
+          currentState = {};
+      } else if (action.type === 'addProperties' && action.extraData) {
+          currentState = { ...currentState, ...action.extraData };
+      } else if (action.type === 'removeProperties' && Array.isArray(action.keysToRemove)) {
+          currentState = Object.fromEntries(
+              Object.entries(currentState).filter(([key]) => !action.keysToRemove.includes(key))
+          );
+      }
+      stateHistory.push(currentState);
+  }
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
